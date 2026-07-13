f4a3aae06e1ee5210986daa30496e51c7b3cf460
AshDhe <157318874+AshDhe@users.noreply.github.com>
2026-07-13 02:36:16 +0200

    Add files via upload

--- ESPACE-PUBLIC/box-menu-burger-public.js
+++ ESPACE-PUBLIC/box-menu-burger-public.js
@@ -313,7 +313,7 @@
 
       const liensPublics = [
         {
-          label: "La Clé",
+          label: "La Clé du Parc",
           href: "/ESPACE-PUBLIC/accueil-public.html"
         },
         {
